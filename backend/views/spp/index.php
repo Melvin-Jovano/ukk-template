865ecd877af3bb5967cfc42c15b08d41c6540a4d
Melvin Jovano <melvinjovano2@gmail.com>
2021-03-22 16:22:01 +0700

    Final Rev

--- backend/views/spp/index.php
+++ backend/views/spp/index.php
@@ -29,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
     ]); ?>
 
     <p class="float-right">
-        <?= Html::a('Buat Data Kelas baru', ['create'], ['class' => 'btn btn-success my-3']) ?>
+        <?= Html::a('Buat Data baru', ['create'], ['class' => 'btn btn-success my-3']) ?>
     </p>
 
 </div>
