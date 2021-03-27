b48656909c164726baf0e9d3ac40d2be41128ebe
Melvin Jovano <melvinjovano2@gmail.com>
2021-03-28 03:33:28 +0700

    Fixed

--- frontend/views/site/dashboard.php
+++ frontend/views/site/dashboard.php
@@ -100,7 +100,7 @@ $this->registerJs('
         // $("#table").slideUp();
         let formData = new FormData;
         $.ajax({
-            url : "/adit_ukk/action/get-all-data",
+            url : "/ardy_ukk/action/get-all-data",
             type : "post",
             data: formData,
             processData: false,
