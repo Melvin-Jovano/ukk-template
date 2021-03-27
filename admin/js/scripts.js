f6cf5b20a06ac62a2845761428de72631eb55081
Melvin Jovano <melvinjovano2@gmail.com>
2021-03-27 14:38:19 +0700

    Fix Pretty URL

---
+++ admin/js/scripts.js
@@ -0,0 +1,73 @@
+/*!
+    * Start Bootstrap - SB Admin v6.0.2 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    (function($) {
+    "use strict";
+
+    // Add active state to sidbar nav links
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+            if (this.href === path) {
+                $(this).addClass("active");
+            }
+        });
+
+    // Toggle the side navigation
+    $("#sidebarToggle").on("click", function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+})(jQuery);
+
+document.onkeyup = function (e) {
+    if (e.ctrlKey && e.which == 66) {
+        $("#search-input").contentEditable = true;
+        $("#search-input").focus();
+    }
+};
+
+$("#search-form").on('submit', (event) => {
+    event.preventDefault();
+    let formData = new FormData;
+    formData.append("name", $("#search-input").val());
+    formData.append("level", $("#user-level").val());
+    $.ajax({
+        url: "/action/get-shortcut",
+        type: "post",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: (data) => {
+            if (!data) {
+                alert("Terjadi Kesalahan");
+            }
+        }
+    });
+});
+
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+    // Strip all characters but numerical ones.
+    number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+    var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+        s = '',
+        toFixedFix = function (n, prec) {
+            var k = Math.pow(10, prec);
+            return '' + Math.round(n * k) / k;
+        };
+    // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+    }
+    if ((s[1] || '').length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
+}
